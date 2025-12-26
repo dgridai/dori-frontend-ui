4884e9a592042791b1b81459de0630704ac58816
metaarch888 <haifun455@gmail.com>
2025-12-26 11:52:23 +0800

    chore: modify page

--- dist/assets/chevron-bottom-cIr72iGg.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-CMquf3wK.js";import"./index-B8Qou4KV.js";import"./index.es-CLoa6zJL.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as chevronBottomSvg};
---
+++ dist/assets/cursor-Dht6g9wZ.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-BbYai6Bj.js";import"./index-wt5WMdR8.js";import"./index.es-CW8YXv_v.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
