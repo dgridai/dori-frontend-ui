c18b6bd54e2132db359bc57fcc26c84cb1c08c9a
metaarch888 <haifun455@gmail.com>
2025-12-26 10:42:38 +0800

    chore: modify link

---
+++ dist/assets/chevron-top-BZlxZLCG.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-CMquf3wK.js";import"./index-B8Qou4KV.js";import"./index.es-CLoa6zJL.js";const i=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{i as chevronTopSvg};
