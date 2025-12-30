2a1a3f3f040ccba72c7502f10d5416228241be04
metaarch888 <haifun455@gmail.com>
2025-12-30 16:52:25 +0800

    chore: modify page

---
+++ dist/assets/all-wallets-DD0RnunP.js
@@ -0,0 +1,6 @@
+import{b as l}from"./core-CxFifhl-.js";import"./index-cyVsixjF.js";import"./index.es-D_6FHA2m.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
