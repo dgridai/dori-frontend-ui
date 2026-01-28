ccdb2a56ae135e856fac105bec77b1e6eef3aae9
metaarch888 <haifun455@gmail.com>
2026-01-28 22:59:21 +0800

    chore: modify reward page  style

--- dist/assets/cursor-8FgUz2sj.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-BFRtqA17.js";import"./index-CRKhOhkW.js";import"./index.es-CjV1u1lB.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
