c71142191b003505fc8747a01d856d73cf7c3a83
metaarch888 <haifun455@gmail.com>
2026-01-28 17:44:26 +0800

    chore: modify api link

--- dist/assets/chevron-top-Br2Tf6EJ.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-9_Sm3Aa9.js";import"./index-B073sgiq.js";import"./index.es-PpV7ugHs.js";const i=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as chevronTopSvg};
