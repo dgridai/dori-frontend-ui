ebfd7d32fe409ac8e48be111f076f7d7a395d6f2
metaarch888 <haifun455@gmail.com>
2026-01-30 18:33:02 +0800

    chore: modify leader page style

---
+++ dist/assets/cursor-hVyOeg-U.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-BdEjjMov.js";import"./index-B7_JkfEo.js";import"./index.es-Cttqpmfp.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
