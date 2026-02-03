cb757d0e934adaabb47337105eb96a4183ed3630
metaarch888 <haifun455@gmail.com>
2026-02-03 18:40:14 +0800

    chore: modify kor language text

---
+++ dist/assets/cursor-BrOE90e2.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-_X2QO9tH.js";import"./index-Ca4Uc02d.js";import"./index.es-B4DwHpFp.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
