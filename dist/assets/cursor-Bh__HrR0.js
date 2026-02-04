e21025fd654b5246f226d45b556e2de66a183b7b
metaarch888 <haifun455@gmail.com>
2026-02-04 16:00:47 +0800

    chore: modify arena page style

---
+++ dist/assets/cursor-Bh__HrR0.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-ChXH_zOh.js";import"./index-D0Q7knSO.js";import"./index.es-DGy_RxLZ.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
