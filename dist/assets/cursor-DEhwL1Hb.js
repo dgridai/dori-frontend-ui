015ff9a088cefd2dc23f610147331bfb468152d8
metaarch888 <haifun455@gmail.com>
2026-02-06 19:22:02 +0800

    chore: modify index style

--- dist/assets/cursor-DEhwL1Hb.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-BqDVvq8o.js";import"./index-BNdfd6em.js";import"./index.es-DDIf_Jf7.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
