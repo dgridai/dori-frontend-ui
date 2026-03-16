3b2c37e968de5b84a0969ca319a764dad87f8146
metaarch888 <haifun455@gmail.com>
2026-03-16 08:06:40 +0800

    chore: modify landing page style

--- dist/assets/cursor-CTSdk6Le.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-D9u-yy3P.js";import"./index-DlYRW6Vx.js";import"./index.es-CKtPAAaW.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
