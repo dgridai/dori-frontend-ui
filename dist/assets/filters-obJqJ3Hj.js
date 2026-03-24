986bb1babfc428ea95e46a95404081f4e4bf22b3
metaarch888 <haifun455@gmail.com>
2026-03-24 16:59:03 +0800

    chore: modify header  style

---
+++ dist/assets/filters-obJqJ3Hj.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-EyO2k3G9.js";import"./index-DvD_CtXy.js";import"./index.es-Lj-w4f-1.js";const a=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M0 3a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1Zm2.63 5.25a1 1 0 0 1 1-1h8.75a1 1 0 1 1 0 2H3.63a1 1 0 0 1-1-1Zm2.62 5.25a1 1 0 0 1 1-1h3.5a1 1 0 0 1 0 2h-3.5a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{a as filtersSvg};
