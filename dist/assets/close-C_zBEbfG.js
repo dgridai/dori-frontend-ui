f55fa4836fa3c61c0e28e323fd378537c0d54fce
metaarch888 <haifun455@gmail.com>
2026-04-29 16:48:22 +0800

    chore: modify model detail style

--- dist/assets/close-C_zBEbfG.js
+++
@@ -1,8 +0,0 @@
-import{w as l}from"./core-Bg880eNG.js";import"./index-BWWVSA2W.js";import"./index.es-CAFABN8q.js";const i=l`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M2.54 2.54a1 1 0 0 1 1.42 0L8 6.6l4.04-4.05a1 1 0 1 1 1.42 1.42L9.4 8l4.05 4.04a1 1 0 0 1-1.42 1.42L8 9.4l-4.04 4.05a1 1 0 0 1-1.42-1.42L6.6 8 2.54 3.96a1 1 0 0 1 0-1.42Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as closeSvg};
