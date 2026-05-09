3ae4fabc16dd050596671354f225dd1863820260
metaarch888 <haifun455@gmail.com>
2026-05-09 18:10:58 +0800

    chore: modify model detail pricing style

---
+++ dist/assets/all-wallets-CRFMV5P5.js
@@ -0,0 +1,6 @@
+import{w as l}from"./core-DlYC9W6m.js";import"./index-BhTjTXab.js";import"./index.es-BgdRwsQD.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
