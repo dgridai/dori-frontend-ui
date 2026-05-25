d7df6b8ce69edd95684f6f6a77589d0f25ba9fde
metaarch888 <haifun455@gmail.com>
2026-05-25 12:10:13 +0800

    chore: modify tags color

---
+++ dist/assets/external-link-DQ0g0U5y.js
@@ -0,0 +1,8 @@
+import{w as l}from"./core-ClGupNBS.js";import"./index-C5Bnxv23.js";import"./index.es-CwUqJZ0E.js";const i=l`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{i as externalLinkSvg};
