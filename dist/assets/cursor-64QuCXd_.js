1b5d0f858a5e40f0267742a5f0cb166cc3391094
metaarch888 <haifun455@gmail.com>
2026-06-02 15:58:47 +0800

    chore: modify model api support range

---
+++ dist/assets/cursor-64QuCXd_.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-9BY4QPyv.js";import"./index-XZfieQ44.js";import"./index.es-Tw3YAD9V.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
