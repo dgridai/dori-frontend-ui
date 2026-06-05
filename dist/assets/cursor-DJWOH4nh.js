07253264c1b707f4d34df37379448cc398b207fa
metaarch888 <haifun455@gmail.com>
2026-06-05 17:52:58 +0800

    chore: modify stats chat style

--- dist/assets/cursor-DJWOH4nh.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-Bj8hwXAv.js";import"./index-CZ9aBtZM.js";import"./index.es-wz-lNq71.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
