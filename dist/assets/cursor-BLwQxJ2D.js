c5f0933ae204218e7f9aabe2144e49aa9a4e6385
metaarch888 <haifun455@gmail.com>
2026-06-22 16:43:56 +0800

    chore: add provider apply modal

--- dist/assets/cursor-BLwQxJ2D.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-C125pG9Y.js";import"./index-zilPrpiP.js";import"./index.es-DLtECeTL.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
