fbf13c9c476f0cf869e3b1e0cee774da6a1be89d
metaarch888 <haifun455@gmail.com>
2026-07-09 11:01:49 +0800

    chore: modify seo config

---
+++ dist/assets/all-wallets-BGcjPH8y.js
@@ -0,0 +1,6 @@
+import{w as l}from"./core-Di1i8ri6.js";import"./index-BQERNiCd.js";import"./index.es-BtKIXf6N.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
