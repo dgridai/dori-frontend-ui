b6debee64e25113f06c43e7e1a13b2dc501b859e
metaarch888 <haifun455@gmail.com>
2026-08-04 17:06:00 +0800

    feat: implement request rules popover and enhance pricing,  add matched request rules localization and enhance pricing logic for channel discounts table with condition handling

---
+++ dist/assets/cursor-D7M-p7Zs.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-Djp--tz6.js";import"./index-DPs3nk_c.js";import"./index.es-BPL0ZRw9.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
